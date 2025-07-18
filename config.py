0617d13816b49daa7b5fd52d2de033a2f63a0ecd
Ubuntu <ubuntu@ip-10-0-2-183.ap-northeast-2.compute.internal>
2025-07-18 01:35:50 +0000

    flask-ec2

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'default_secret')
+    # MySQL RDS 연결 문자열 예시: 'mysql+pymysql://username:password@host:3306/dbname'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URI', 'mysql+pymysql://user:password@host:3306/dbname')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_REGION = os.environ.get('AWS_REGION', 'ap-northeast-2')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+    DYNAMODB_TABLE = os.environ.get('DYNAMODB_TABLE', 'chat_data') 
