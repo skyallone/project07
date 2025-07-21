8eea33469a3e28bc7e45a21910e5f719260b2144
Ubuntu <ubuntu@ip-10-0-2-183.ap-northeast-2.compute.internal>
2025-07-21 11:39:04 +0000

    terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,1 @@
-variable "cluster_name" {
-  description = "EKS Cluster name"
-  type        = string
-  default     = "projectflask"
-}
-
-variable "node_group_name" {
-  description = "EKS Node Group name"
-  type        = string
-  default     = "project-node-group"
-}
-
-variable "node_instance_type" {
-  description = "EC2 instance type for EKS nodes"
-  type        = string
-  default     = "t3.medium"
-} 
+ 
