79da69b9db8cf60df6c75fdd8cc06fe5296e3337
Ubuntu <ubuntu@ip-10-0-2-183.ap-northeast-2.compute.internal>
2025-07-22 00:45:42 +0000

    terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "project-terraform25"
+    key            = "state/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-2"
 } 
